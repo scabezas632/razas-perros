450e2e1d8edb10b08beed8d369cc2849b0cf1626
scabezas632 <samuel.cabezas.632@gmail.com>
2018-06-16 01:09:59 -0400

    Se añade componente de razas para el menu que contendra filtros de busqueda

---
+++ src/app/razas/razas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-razas',
+  templateUrl: './razas.component.html',
+  styleUrls: ['./razas.component.css']
+})
+export class RazasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
