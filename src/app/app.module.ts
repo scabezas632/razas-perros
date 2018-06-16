2e8139130624defff0d4922de4cb5e6179f00bf1
scabezas632 <samuel.cabezas.632@gmail.com>
2018-06-16 12:38:26 -0400

    Se agrupan los componentes en la carpeta pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { RazasComponent } from './razas/razas.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { NoPageFoundComponent } from './shared/no-page-found/no-page-found.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RazasComponent,
     HomeComponent,
-    AboutComponent,
     ContactComponent,
     NavbarComponent,
+    NoPageFoundComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
